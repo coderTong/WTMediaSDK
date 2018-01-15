62d81fe60bd96baa51f49373f4ed3951e616ad75
wutong <tong.wu@netviewtech.com>
2018-01-15 15:38:13 +0800

    Init prj

---
+++ WTMediaSDK/WTMediaSDK.h
@@ -0,0 +1,19 @@
+//
+//  WTMediaSDK.h
+//  WTMediaSDK
+//
+//  Created by codew on 2018/1/15.
+//  Copyright © 2018年 codew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WTMediaSDK.
+FOUNDATION_EXPORT double WTMediaSDKVersionNumber;
+
+//! Project version string for WTMediaSDK.
+FOUNDATION_EXPORT const unsigned char WTMediaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WTMediaSDK/PublicHeader.h>
+
+
